fcf9191e66cae7b0c4162ce985eead5a545f2c49
Sain13 <150769804+Sain13@users.noreply.github.com>
2025-02-12 13:09:52 +0500

    Update delCart.js

--- js/delCart.js
+++ js/delCart.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const userNameElement = document.querySelector(".login-text");
-    let cart = document.getElementById("carta"); // Исправлено getElementById
-
-    if (userNameElement) {
-        const userNameText = userNameElement.textContent.trim(); // Убираем лишние пробелы
-
+    let cart = document.getElementById("carta"); // Исправлено getElementByI
         // Если текст отличается от "Войти" и не пустой, меняем кнопку
-        if (userNameText !== "Войти" && userNameText !== "") {
-            if (cart) {
-                cart.remove(); // Удаление элемента
-            }
+    if (userNameText !== "Войти" && userNameText !== "") {
+        if (cart) {
+            cart.remove(); // Удаление элемента
         }
+    }
     }
 });
