3313fa1b96fa5d55077bfd753e9d828365824e48
Sain13 <150769804+Sain13@users.noreply.github.com>
2025-02-12 13:51:01 +0600

    Update delCart.js

--- js/delCart.js
+++ js/delCart.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
- let cart = document.getElementById("carta"); 
- if (userNameText !== "Войти" && userNameText !== "") {
-  cart.remove(); }
-} );
+  let cart = document.getElementById("carta"); 
+  let userNameText = document.getElementById("userName").innerText;  // Получаем значение из нужного элемента
+  if (userNameText !== "Войти" && userNameText !== "") {
+    if (cart) { // Проверяем, существует ли элемент
+      cart.remove(); 
+    }
+  }
+});
